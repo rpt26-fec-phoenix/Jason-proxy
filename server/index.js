05b76fb483d0225b2d51afe9df410159d9364e06
jasonschreib <jwschreiber1@gmail.com>
2021-03-20 14:12:14 -0700

    Finished index.html and working on sending get requests through server

--- server/index.js
+++ server/index.js
@@ -5,7 +5,15 @@ const port = 3000;
 //app.use on the propertyID url
 app.use(':propertyID', express.static(__dirname + '/../dist'));
 
+//send get request to title service
+
 //send get request to photos service
+
+//send get request to details service
+
+//send get request to calendar component
+
+//send get request to reviews service
 
 app.listen(port, () => {
   console.log(`Proxy server running on http://localhost:${port}`);
