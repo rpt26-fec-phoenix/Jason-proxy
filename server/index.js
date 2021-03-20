08f3a80849ee26a764f68b825298ef7544fcb908
jasonschreib <jwschreiber1@gmail.com>
2021-03-20 14:37:48 -0700

    photos service rendering through proxy

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
+const axios = require('axios');
 const express = require ('express');
 const app = express();
 const port = 3000;
 
+
 //app.use on the propertyID url
-app.use(':propertyID', express.static(__dirname + '/../dist'));
+app.use('/:propertyID', express.static(__dirname + '/../public'));
 
 //send get request to title service
 
 //send get request to photos service
+app.get('/photos/:propertyID', (req, res) => {
+  axios.get(`http://localhost:3002/photos/${req.params.propertyID}`)
+  .then((photos) => {
+    res.send(photos.data);
+  })
+  .catch((err) => {
+    res.send(err);
+  })
+});
 
 //send get request to details service
 
