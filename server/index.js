55ba83850881eae5a1df04ea81c0af4781a1401c
jasonschreib <jwschreiber1@gmail.com>
2021-03-22 00:04:40 -0700

    working on getting checkout info and reviews to render in client

--- server/index.js
+++ server/index.js
@@ -32,8 +32,36 @@ app.get('/details/:propertyID', (req, res) => {
 });
 
 //send get request to checkout/calendar component
+app.get('/checkoutInformation/:productID', (req, res) => {
+  axios.get(`http://localhost:3004/checkoutInformation/${req.params.productID}`)
+  .then((info) => {
+    res.send(info.data);
+  })
+  .catch((err) => {
+    res.send(err);
+  })
+})
 
-//send get request to reviews service
+//send get request to reviews service - 2 requests
+// app.get('/:listingID/reviews', (req, res) => {
+//   axios.get(`http://localhost:3006/${req.params.listingID}/reviews`)
+//   .then((reviews) => {
+//     res.send(reviews.data);
+//   })
+//   .catch((err) => {
+//     res.send(err);
+//   })
+// });
+
+// app.get('/:listingID/averageReviewsRating', (req, res) => {
+//   axios.get(`http://localhost:3003/${req.params.listingID}/averageReviewsRating`)
+//   .then((avgReviews) => {
+//     res.send(avgReviews.data);
+//   })
+//   .catch((err) => {
+//     res.send(err);
+//   })
+// });
 
 app.listen(port, () => {
   console.log(`Proxy server running on http://localhost:${port}`);
